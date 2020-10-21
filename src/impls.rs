0112f03828d230076b7485518e24deba809e26f8
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-10-21 14:42:52 +0200

    impl Format for tuples

--- src/impls.rs
+++ src/impls.rs
@@ -263,6 +263,52 @@ where
     }
 }
 
+impl Format for () {
+    fn format(&self, f: &mut Formatter) {
+        if f.needs_tag() {
+            let t = internp!("()");
+            f.u8(&t);
+        }
+    }
+}
+
+macro_rules! tuple {
+    ( $format:expr, ($($name:ident),+) ) => (
+        impl<$($name:Format),+> Format for ($($name,)+) where last_type!($($name,)+): ?Sized {
+            #[allow(non_snake_case, unused_assignments)]
+            fn format(&self, f: &mut Formatter) {
+                if f.needs_tag() {
+                    let t = internp!($format);
+                    f.u8(&t);
+                }
+
+                let ($(ref $name,)+) = *self;
+                $(
+                    $name.format(f);
+                )+
+            }
+        }
+    )
+}
+
+macro_rules! last_type {
+    ($a:ident,) => { $a };
+    ($a:ident, $($rest_a:ident,)+) => { last_type!($($rest_a,)+) };
+}
+
+tuple! { "({:?})", (T0) }
+tuple! { "({:?}, {:?})", (T0, T1) }
+tuple! { "({:?}, {:?}, {:?})", (T0, T1, T2) }
+tuple! { "({:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5, T6) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5, T6, T7) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5, T6, T7, T8) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) }
+tuple! { "({:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?})", (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) }
+
 #[cfg(feature = "alloc")]
 mod if_alloc {
     use crate::{Format, Formatter};
