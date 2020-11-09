c74cdc73f9c0cd7a052ddda4d1d8e08784c5eb78
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2020-11-09 13:45:37 +0100

    Handle crates.io versions in decoder build script

--- decoder/build.rs
+++ decoder/build.rs
@@ -1,9 +1,24 @@
 use std::{env, error::Error, fs, path::PathBuf, process::Command};
 
+use semver::Version;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let out = &PathBuf::from(env::var("OUT_DIR")?);
     let output = Command::new("git").args(&["rev-parse", "HEAD"]).output()?;
-    let commit = String::from_utf8(output.stdout).unwrap();
+    let version = if output.status.success() {
+        String::from_utf8(output.stdout).unwrap()
+    } else {
+        // no git info -> assume crates.io
+        let semver = Version::parse(&std::env::var("CARGO_PKG_VERSION")?)?;
+        if semver.major == 0 {
+            // minor is breaking when major = 0
+            format!("{}.{}", semver.major, semver.minor)
+        } else {
+            // ignore minor, patch, pre and build
+            semver.major.to_string()
+        }
+    };
+
     fs::write(
         out.join("version.rs"),
         format!(
@@ -11,7 +26,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 /// Supported `defmt` wire format
 const DEFMT_VERSION: &str = "{}";
 "#,
-            commit.trim(),
+            version.trim(),
         ),
     )?;
     Ok(())
