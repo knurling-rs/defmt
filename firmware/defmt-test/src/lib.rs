c117ae2300d78e4f93add723c88355faba7b762b
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-11-16 15:30:36 +0100

    Apply suggestions from code review
    Co-authored-by: Jonas Schievink <jonas.schievink@ferrous-systems.com>

--- firmware/defmt-test/src/lib.rs
+++ firmware/defmt-test/src/lib.rs
@@ -1,10 +1,10 @@
-//! A test harness for embedded devices
+//! A test harness for embedded devices.
 //!
 //! This crate has a single API: the `#[tests]` macro. This macro is documented in the project
 //! README which can be found at:
 //!
-//! - https://crates.io/crates/defmt (crates.io version)
-//! - https://github.com/knurling-rs/defmt/tree/main/firmware/defmt-test (git version)
+//! - <https://crates.io/crates/defmt-test> (crates.io version)
+//! - <https://github.com/knurling-rs/defmt/tree/main/firmware/defmt-test> (git version)
 
 #![no_std]
 
