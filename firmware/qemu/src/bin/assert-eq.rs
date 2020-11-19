c2c335a51956c6b74d47684cb5055eddd41a30ff
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-11-19 14:49:24 +0100

    add snapshot tests

---
+++ firmware/qemu/src/bin/assert-eq.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::debug;
+
+use defmt_semihosting as _; // global logger
+
+#[entry]
+fn main() -> ! {
+    let x = 42;
+    defmt::debug_assert_eq!(x - 1, x + 1, "dev");
+    defmt::assert_eq!(x - 1, x + 1, "release");
+    defmt::unreachable!();
+}
+
+// like `panic-semihosting` but doesn't print to stdout (that would corrupt the defmt stream)
+#[cfg(target_os = "none")]
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {
+        debug::exit(debug::EXIT_SUCCESS)
+    }
+}
--- firmware/qemu/test.sh
+++ firmware/qemu/test.sh
@@ -5,12 +5,14 @@ set -o errexit
 function test() {
     local bin=$1
     local features=${2-,}
-    
+
     cargo rb "$bin" --features "$features" | tee "src/bin/$bin.out.new" | diff "src/bin/$bin.out" -
     cargo rrb "$bin" --features "$features" | tee "src/bin/$bin.release.out.new" | diff "src/bin/$bin.release.out" -
 }
 
 test "log"
+test "panic"
+test "assert"
 if rustc -V | grep nightly; then
     test "alloc" "alloc"
 fi
