f86e1e41c6f1173de550a0bb1248f1094987050f
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2021-01-12 16:52:32 +0100

    add defmt-logger and defmt-print
    defmt-logger is probe-run's code that formats defmt frames into a log::Logger sink
    defmt-print is a command-line tool that decodes defmt and prints it to the console

---
+++ print/README.md
@@ -0,0 +1,37 @@
+# `defmt-print`
+
+> A tool to decode and print [`defmt`] data
+
+[`defmt`]: https://crates.io/crates/defmt
+
+There's no stable library API to decode `defmt` log frames but this tool can be used to decode defmt
+data and print it to the console.
+
+## Support
+
+`defmt-print` is part of the [Knurling] project, [Ferrous Systems]' effort at
+improving tooling used to develop for embedded systems.
+
+If you think that our work is useful, consider sponsoring it via [GitHub
+Sponsors].
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+licensed as above, without any additional terms or conditions.
+
+[Knurling]: https://knurling.ferrous-systems.com/
+[Ferrous Systems]: https://ferrous-systems.com/
+[GitHub Sponsors]: https://github.com/sponsors/knurling-rs
---
+++ print/src/main.rs
@@ -0,0 +1,91 @@
+use std::{
+    env, fs,
+    io::{self, Read},
+    path::PathBuf,
+};
+
+use anyhow::anyhow;
+use structopt::StructOpt;
+
+/// Prints defmt-encoded logs to stdout
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(short, parse(from_os_str))]
+    elf: PathBuf,
+    // may want to add this later
+    // #[structopt(short, long)]
+    // verbose: bool,
+    // TODO add file path argument; always use stdin for now
+}
+
+const READ_BUFFER_SIZE: usize = 1024;
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::from_args();
+    let verbose = false;
+    defmt_logger::init(verbose);
+
+    let bytes = fs::read(&opts.elf)?;
+
+    let table = defmt_elf2table::parse(&bytes)?.ok_or_else(|| anyhow!(".defmt data not found"))?;
+    let locs = defmt_elf2table::get_locations(&bytes, &table)?;
+
+    let locs = if table.indices().all(|idx| locs.contains_key(&(idx as u64))) {
+        Some(locs)
+    } else {
+        log::warn!("(BUG) location info is incomplete; it will be omitted from the output");
+        None
+    };
+
+    let mut buf = [0; READ_BUFFER_SIZE];
+    let mut frames = vec![];
+
+    let current_dir = env::current_dir()?;
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    loop {
+        let n = stdin.read(&mut buf)?;
+
+        frames.extend_from_slice(&buf[..n]);
+
+        loop {
+            match defmt_decoder::decode(&frames, &table) {
+                Ok((frame, consumed)) => {
+                    // NOTE(`[]` indexing) all indices in `table` have already been
+                    // verified to exist in the `locs` map
+                    let loc = locs.as_ref().map(|locs| &locs[&frame.index()]);
+
+                    let (mut file, mut line, mut mod_path) = (None, None, None);
+                    if let Some(loc) = loc {
+                        let relpath = if let Ok(relpath) = loc.file.strip_prefix(&current_dir) {
+                            relpath
+                        } else {
+                            // not relative; use full path
+                            &loc.file
+                        };
+                        file = Some(relpath.display().to_string());
+                        line = Some(loc.line as u32);
+                        mod_path = Some(loc.module.clone());
+                    }
+
+                    // Forward the defmt frame to our logger.
+                    defmt_logger::log_defmt(
+                        &frame,
+                        file.as_deref(),
+                        line,
+                        mod_path.as_ref().map(|s| &**s),
+                    );
+
+                    let num_frames = frames.len();
+                    frames.rotate_left(consumed);
+                    frames.truncate(num_frames - consumed);
+                }
+                Err(defmt_decoder::DecodeError::UnexpectedEof) => break,
+                Err(defmt_decoder::DecodeError::Malformed) => {
+                    log::error!("failed to decode defmt data: {:x?}", frames);
+                    Err(defmt_decoder::DecodeError::Malformed)?
+                }
+            }
+        }
+    }
+}
