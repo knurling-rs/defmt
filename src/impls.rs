99928b18c4bcd162d4e21b8ae953d9ca14807b1b
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2021-02-10 14:51:33 +0100

    impl Format for Infallible

--- src/impls.rs
+++ src/impls.rs
@@ -450,3 +450,10 @@ mod if_alloc {
         }
     }
 }
+
+impl Format for core::convert::Infallible {
+    fn format(&self, _: Formatter) {
+        // type cannot be instantiated so nothing to do here
+        match *self {}
+    }
+}
