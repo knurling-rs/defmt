25ee5c41df9012a4629a116c28d26d44fc159764
Anatol Ulrich <anatol.ulrich@ferrous-systems.com>
2021-02-16 01:49:41 +0100

    theoretically feature complete, HOWEVER heisenbug

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -8,7 +8,87 @@ use std::{io::Read, path::Path};
 use console::Style;
 use similar::{ChangeTag, TextDiff};
 
-use anyhow::{anyhow, Context, Error, Result};
+use anyhow::{anyhow, Context, Result};
+
+fn run_command<P: AsRef<Path>>(cmd_and_args: &[&str], cwd: Option<P>) -> Result<()> {
+    println!("start {:?}", cmd_and_args);
+    let cmd_and_args = Vec::from(cmd_and_args);
+    let mut cmd = &mut Command::new(cmd_and_args[0]);
+    if cmd_and_args.len() > 1 {
+        cmd.args(&cmd_and_args[1..]);
+    }
+
+    let cwd_s: String;
+    match cwd {
+        Some(path_ref) => {
+            cmd = cmd.current_dir(path_ref.as_ref());
+
+            cwd_s = format!(
+                "{}{}",
+                path_ref
+                    .as_ref()
+                    .to_str()
+                    .unwrap_or("(non-Unicode path)")
+                    .to_string(),
+                std::path::MAIN_SEPARATOR
+            );
+        }
+        None => {
+            cwd_s = "".to_string();
+        }
+    }
+
+    let cmdline = cmd_and_args.join(" ");
+    cmd.status()
+        .map_err(|e| anyhow!("could not run '{}{}': {}", cwd_s, cmdline, e))
+        .and_then(|exit_status| {
+            if exit_status.success() {
+                println!("OK {:?}", cmd_and_args);
+                Ok(())
+            } else {
+                let info = match exit_status.code() {
+                    Some(code) => {
+                        format!("non-zero exit status: {}", code)
+                    }
+                    None => "killed by signal".to_string(),
+                };
+
+                println!("NOT OK {:?}, {}", cmdline, info);
+                Err(anyhow!(
+                    "'{}' did not finish successfully: {}",
+                    cmdline,
+                    info
+                ))
+            }
+        })
+}
+
+fn run_capturing_stdout(cmd: &mut Command) -> Result<String> {
+    let child = cmd.stdout(Stdio::piped()).spawn()?;
+    let mut stdout = child
+        .stdout
+        .ok_or(anyhow!("could not access standard output"))?;
+    let mut out = String::new();
+    stdout
+        .read_to_string(&mut out)
+        .with_context(|| "could not read standard output")?;
+    Ok(out)
+}
+
+fn do_test<F>(t: F, context: &str, errors: &mut Vec<String>)
+where
+    F: FnOnce() -> Result<()>,
+{
+    match t() {
+        Ok(_) => {}
+        Err(e) => errors.push(format!("{}: {}", context, e)),
+    }
+}
+
+fn rustc_is_nightly() -> Result<bool> {
+    let out = run_capturing_stdout(Command::new("rustc").args(&["-V"]))?;
+    Ok(out.contains("nightly"))
+}
 
 fn load_expected_output(name: &str, release_mode: bool) -> Result<String> {
     let path = Path::new("firmware/qemu/src/bin");
@@ -23,46 +103,32 @@ fn load_expected_output(name: &str, release_mode: bool) -> Result<String> {
     let path = path.join(filename);
 
     // for error context closure
-    let path_str = path.to_str().unwrap_or("(non-Unicode path)").to_owned();
+    let path_str = path.to_str().unwrap_or("(non-Unicode path)").to_string();
 
     let content = fs::read_to_string(path)
         .with_context(|| format!("Failed to load expected output data from {}", path_str))?;
     Ok(content)
 }
 
-fn run_returning_stdout(cmd: &mut Command) -> Result<String> {
-    let child = cmd.stdout(Stdio::piped()).spawn()?;
-    let mut stdout = child
-        .stdout
-        .ok_or(anyhow!("could not access standard output"))?;
-    let mut out = String::new();
-    stdout
-        .read_to_string(&mut out)
-        .with_context(|| "could not read standard output")?;
-    Ok(out)
-}
-
-fn test_qemu(name: &str, features: &str, release_mode: bool) -> Result<()> {
+fn test_single_snapshot(name: &str, features: &str, release_mode: bool) -> Result<()> {
     let display_name = format!(
         "{} ({})",
         name,
         if release_mode { "release" } else { "dev" }
     );
     println!("{}", display_name);
-    let cwd_name = "firmware/qemu".to_owned();
-    let cwd = fs::canonicalize(&cwd_name)
-        .map_err(|e| anyhow!("running {} in {}: {}", display_name, cwd_name, e))?;
+    let cwd_name = "firmware/qemu".to_string();
     let mut args;
     if release_mode {
         args = vec!["-q", "rrb", name]
     } else {
         args = vec!["-q", "rb", name]
     }
-    if features.len() > 0 {
+    if !features.is_empty() {
         args.extend_from_slice(&["--features", features]);
     }
 
-    let actual = run_returning_stdout(Command::new("cargo").args(&args).current_dir(cwd))?;
+    let actual = run_capturing_stdout(Command::new("cargo").args(&args).current_dir(cwd_name))?;
 
     let expected = load_expected_output(name, release_mode)?;
 
@@ -94,19 +160,251 @@ fn test_qemu(name: &str, features: &str, release_mode: bool) -> Result<()> {
     }
 }
 
-fn run_test<F>(t: F, context: &str, errors: &mut Vec<String>) -> ()
-where
-    F: FnOnce() -> Result<()>,
-{
-    match t() {
-        Ok(_) => {}
-        Err(e) => errors.push(format!("{}: {}", context, e)),
+fn get_installed_targets() -> Result<HashSet<String>> {
+    let out = run_capturing_stdout(Command::new("rustup").args(&["target", "list"]))?;
+    let installed_marker = " (installed)";
+    let mut targets = out.lines().collect::<Vec<_>>();
+    targets.retain(|target| target.contains(installed_marker));
+    let targets: HashSet<String> = targets
+        .iter()
+        .map(|target| target.replace(installed_marker, ""))
+        .collect();
+    Ok(targets)
+}
+
+fn install_targets() -> Result<Vec<String>> {
+    println!("installing targets");
+
+    let required_targets = vec![
+        "thumbv6m-none-eabi",
+        "thumbv7m-none-eabi",
+        "thumbv7em-none-eabi",
+        "thumbv8m.base-none-eabi",
+        "riscv32i-unknown-none-elf",
+    ];
+    let all_targets = required_targets
+        .iter()
+        .map(|item| item.to_string())
+        .collect::<HashSet<_>>();
+
+    let installed_targets = get_installed_targets()?;
+    let missing_targets = all_targets.difference(&installed_targets);
+    let mut added_targets: Vec<String> = vec![];
+
+    // since installing targets is the first thing we do, hard panic is fine
+
+    for target in missing_targets {
+        let status = Command::new("rustup")
+            .args(&["target", "add", target])
+            .status()
+            .unwrap();
+        if !status.success() {
+            panic!("Error installing target: {}", target);
+        } else {
+            println!("added target {}", target);
+        }
+        added_targets.push(target.to_owned());
+    }
+
+    Ok(added_targets)
+}
+
+fn uninstall_targets(targets: Vec<String>) {
+    println!("uninstalling targets");
+
+    // print all uninstall errors so the user can fix those manually if needed
+    for target in targets {
+        match run_command::<&str>(&["rustup", "target", "remove", &target], None) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error uninstalling target {}: {}", target, e);
+            }
+        }
     }
 }
 
-fn rustc_is_nightly() -> Result<bool> {
-    let out = run_returning_stdout(Command::new("rustc").args(&["-V"]))?;
-    Ok(out.contains("nightly"))
+fn test_book(errors: &mut Vec<String>) -> () {
+    println!("*** book ***");
+    do_test(
+        || run_command::<&str>(&["cargo", "clean"], None),
+        "book",
+        errors,
+    );
+
+    do_test(
+        || run_command::<&str>(&["cargo", "build", "--features", "unstable-test"], None),
+        "book",
+        errors,
+    );
+
+    do_test(
+        || {
+            run_command(
+                &[
+                    "mdbook",
+                    "test",
+                    "-L",
+                    "../target/debug",
+                    "-L",
+                    "../target/debug/deps",
+                ],
+                Some("book"),
+            )
+        },
+        "book",
+        errors,
+    );
+}
+
+fn test_lint(errors: &mut Vec<String>) -> () {
+    println!("*** lint ***");
+    do_test(
+        || run_command::<&str>(&["cargo", "fmt", "--all", "--", "--check"], None),
+        "lint",
+        errors,
+    );
+
+    do_test(
+        || run_command::<&str>(&["cargo", "clippy", "--workspace"], None),
+        "lint",
+        errors,
+    );
+}
+
+fn test_host(errors: &mut Vec<String>) -> () {
+    println!("*** host ***");
+
+    do_test(
+        || {
+            run_command::<&str>(
+                &[
+                    "cargo",
+                    "test",
+                    "--workspace",
+                    "--features",
+                    "unstable-test",
+                ],
+                None,
+            )
+        },
+        "host",
+        errors,
+    );
+}
+
+fn test_cross(errors: &mut Vec<String>) -> () {
+    println!("*** cross ***");
+    let targets = vec![
+        "thumbv6m-none-eabi",
+        "thumbv8m.base-none-eabi",
+        "riscv32i-unknown-none-elf",
+    ];
+
+    for target in targets {
+        do_test(
+            || {
+                run_command::<&str>(
+                    &["cargo", "check", "--target", &target, "-p", "defmt"],
+                    None,
+                )
+            },
+            "cross",
+            errors,
+        );
+        do_test(
+            || {
+                run_command::<&str>(
+                    &[
+                        "cargo",
+                        "check",
+                        "--target",
+                        &target,
+                        "-p",
+                        "defmt",
+                        "--features",
+                        "alloc",
+                    ],
+                    None,
+                )
+            },
+            "cross",
+            errors,
+        );
+    }
+
+    do_test(
+        || {
+            run_command(
+                &[
+                    "cargo",
+                    "check",
+                    "--target",
+                    "thumbv6m-none-eabi",
+                    "--workspace",
+                    "--exclude",
+                    "defmt-itm",
+                    "--exclude",
+                    "firmware",
+                ],
+                Some("firmware"),
+            )
+        },
+        "cross",
+        errors,
+    );
+
+    do_test(
+        || {
+            run_command(
+                &[
+                    "cargo",
+                    "check",
+                    "--target",
+                    "thumbv7em-none-eabi",
+                    "--workspace",
+                ],
+                Some("firmware"),
+            )
+        },
+        "cross",
+        errors,
+    );
+
+    do_test(
+        || {
+            run_command(
+                &[
+                    "cargo",
+                    "check",
+                    "--target",
+                    "thumbv6m-none-eabi",
+                    "--features",
+                    "print-defmt",
+                ],
+                Some("firmware/panic-probe"),
+            )
+        },
+        "cross",
+        errors,
+    );
+
+    do_test(
+        || {
+            run_command::<&str>(
+                &[
+                    "cargo",
+                    "check",
+                    "--target",
+                    "thumbv6m-none-eabi",
+                    "--features",
+                    "print-rtt",
+                ],
+                Some("firmware/panic-probe"),
+            )
+        },
+        "cross",
+        errors,
+    )
 }
 
 fn test_snapshot(errors: &mut Vec<String>) -> () {
@@ -144,67 +442,16 @@ fn test_snapshot(errors: &mut Vec<String>) -> () {
     for test in &tests {
         let features = features_map.get(test).unwrap_or(&no_features);
 
-        run_test(|| test_qemu(test, features, false), "qemu/snapshot", errors);
-        run_test(|| test_qemu(test, features, true), "qemu/snapshot", errors);
-    }
-}
-
-fn get_installed_targets() -> Result<HashSet<String>> {
-    let out = run_returning_stdout(Command::new("rustup").args(&["target", "list"]))?;
-    let installed_marker = " (installed)";
-    let mut targets = out.lines().collect::<Vec<_>>();
-    targets.retain(|target| target.contains(installed_marker));
-    let targets: HashSet<String> = targets
-        .iter()
-        .map(|target| target.replace(installed_marker, ""))
-        .collect();
-    Ok(targets)
-}
-
-fn install_targets() -> Result<Vec<String>> {
-    println!("installing targets");
-    let required_targets = vec![
-        "thumbv6m-none-eabi",
-        "thumbv7m-none-eabi",
-        "thumbv7em-none-eabi",
-        "thumbv8m.base-none-eabi",
-        "riscv32i-unknown-none-elf",
-    ];
-    let all_targets = required_targets
-        .iter()
-        .map(|item| item.to_string())
-        .collect::<HashSet<_>>();
-
-    let installed_targets = get_installed_targets()?;
-    let missing_targets = all_targets.difference(&installed_targets);
-    let mut added_targets = vec![];
-
-    // since installing targets is the first thing we do, hard panic is fine
-    for target in missing_targets {
-        let status = Command::new("rustup")
-            .args(&["target", "add", target])
-            .status()
-            .unwrap();
-        if !status.success() {
-            panic!("Error installing target: {}", target);
-        }
-        added_targets.push(target.to_owned());
-    }
-
-    Ok(added_targets)
-}
-
-fn uninstall_targets(targets: Vec<String>) {
-    println!("uninstalling targets");
-
-    // print all uninstall errors so the user can fix those manually if needed
-    for target in targets {
-        match run_command(&["rustup", "target", "remove", &target]) {
-            Ok(_) => {}
-            Err(e) => {
-                eprintln!("Error uninstalling target {}: {}", target, e);
-            }
-        }
+        do_test(
+            || test_single_snapshot(test, features, false),
+            "qemu/snapshot",
+            errors,
+        );
+        do_test(
+            || test_single_snapshot(test, features, true),
+            "qemu/snapshot",
+            errors,
+        );
     }
 }
 
@@ -233,7 +480,7 @@ fn main() -> Result<(), String> {
             test_lint(&mut all_errors);
 
             if !all_errors.is_empty() {
-                eprintln!("");
+                eprintln!();
                 eprintln!("some tests failed:");
                 for error in all_errors {
                     eprintln!("{}", error);
@@ -253,68 +500,3 @@ fn main() -> Result<(), String> {
         }
     }
 }
-
-fn test_lint(all_errors: &mut Vec<String>) -> () {}
-
-fn test_book(all_errors: &mut Vec<String>) -> () {}
-
-fn run_command(cmd_and_args: &[&str]) -> Result<()> {
-    let cmd_and_args = Vec::from(cmd_and_args);
-    let mut cmd = Command::new(cmd_and_args[0]);
-    if cmd_and_args.len() > 1 {
-        cmd.args(&cmd_and_args[1..]);
-    }
-
-    let cmdline = cmd_and_args.join(" ");
-    cmd.status()
-        .map_err(|e| anyhow!("could not run '{}': {}", cmdline, e))
-        .and_then(|exit_status| {
-            if exit_status.success() {
-                Ok(())
-            } else {
-                Err(anyhow!(
-                "'{}' did not finish successfully (non-zero exit status or killed by signal): {:?}",
-                cmdline,
-                exit_status.code()
-            ))
-            }
-        })
-}
-
-fn test_cross(errors: &mut Vec<String>) -> () {
-    println!("*** cross ***");
-    let targets = vec![
-        "thumbv6m-none-eabi",
-        "thumbv8m.base-none-eabi",
-        "riscv32i-unknown-none-elf",
-    ];
-
-    for target in targets {
-        run_test(
-            || run_command(&["cargo", "check", "--target", &target, "-p", "defmt"]),
-            "cross",
-            errors,
-        );
-        run_test(
-            || {
-                run_command(&[
-                    "cargo",
-                    "check",
-                    "--target",
-                    &target,
-                    "-p",
-                    "defmt",
-                    "--features",
-                    "alloc",
-                ])
-            },
-            "cross",
-            errors,
-        );
-    }
-
-    //cargo check --target $target -p defmt
-    //cargo check --target $target -p defmt --features alloc
-}
-
-fn test_host(all_errors: &mut Vec<String>) -> () {}
