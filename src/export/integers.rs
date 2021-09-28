f39d33096a382f00627b7ca1cc35710c03abb112
Johann Hemmann <johann.hemmann@code.berlin>
2021-09-28 12:50:39 +0200

    `defmt::export`: Repetition for `impl_truncate!` and `write_to_le_bytes!`

--- src/export/integers.rs
+++ src/export/integers.rs
@@ -1,30 +1,20 @@
 use super::*;
 
 macro_rules! write_to_le_bytes {
-    ($s:ident) => {
-        /// Implementation detail
+    ($($s:ident),*) => {
+        $(/// Implementation detail
         pub fn $s(b: &$s) {
             write(&b.to_le_bytes())
-        }
+        })*
     };
 }
 
-write_to_le_bytes!(u8);
-write_to_le_bytes!(u16);
-write_to_le_bytes!(u32);
-write_to_le_bytes!(u64);
-write_to_le_bytes!(u128);
+write_to_le_bytes!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128);
 
 /// Implementation detail
 pub fn usize(b: &usize) {
     write(&(*b as u32).to_le_bytes())
 }
-
-write_to_le_bytes!(i8);
-write_to_le_bytes!(i16);
-write_to_le_bytes!(i32);
-write_to_le_bytes!(i64);
-write_to_le_bytes!(i128);
 
 /// Implementation detail
 pub fn isize(b: &isize) {
--- src/export/sealed.rs
+++ src/export/sealed.rs
@@ -7,33 +7,34 @@ pub trait Truncate<U> {
 }
 
 macro_rules! impl_truncate {
-    ($t:ty => $u:ty) => {
-        impl Truncate<$t> for $u {
+    ($($t:ty => $u:ty),*) => {
+        $(impl Truncate<$t> for $u {
             fn truncate(self) -> $t {
                 self as $t
             }
-        }
+        })*
     };
 }
 
-// We implement Truncate<X> for X so that the macro can unconditionally use it,
+// We implement `Truncate<X> for X` so that the macro can unconditionally use it,
 // even if no truncation is performed.
-
-impl_truncate!(u8 => u8);
-impl_truncate!(u8 => u16);
-impl_truncate!(u8 => u32);
-impl_truncate!(u8 => u64);
-impl_truncate!(u8 => u128);
-impl_truncate!(u16 => u16);
-impl_truncate!(u16 => u32);
-impl_truncate!(u16 => u64);
-impl_truncate!(u16 => u128);
-impl_truncate!(u32 => u32);
-impl_truncate!(u32 => u64);
-impl_truncate!(u32 => u128);
-impl_truncate!(u64 => u64);
-impl_truncate!(u64 => u128);
-impl_truncate!(u128 => u128);
+impl_truncate!(
+    u8   => u8,
+    u8   => u16,
+    u8   => u32,
+    u8   => u64,
+    u8   => u128,
+    u16  => u16,
+    u16  => u32,
+    u16  => u64,
+    u16  => u128,
+    u32  => u32,
+    u32  => u64,
+    u32  => u128,
+    u64  => u64,
+    u64  => u128,
+    u128 => u128
+);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct NoneError;
