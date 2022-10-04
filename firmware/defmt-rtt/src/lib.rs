bda235a67ca0db0030f24420935cb3ea8d71106a
Urhengulas <johann.hemmann@code.berlin>
2022-10-04 13:09:33 +0200

    Group `mod` statements

--- firmware/defmt-rtt/src/lib.rs
+++ firmware/defmt-rtt/src/lib.rs
@@ -34,12 +34,11 @@
 #![no_std]
 
 mod channel;
+mod consts;
 
 use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 
 use crate::channel::Channel;
-
-mod consts;
 
 /// RTT buffer size. Default: 1024; can be customized by setting the `DEFMT_RTT_BUFFER_SIZE` environment variable.
 /// Use a power of 2 for best performance.
