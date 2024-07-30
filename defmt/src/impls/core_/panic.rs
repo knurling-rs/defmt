75099f96c838f85346f63fa00c165692aedc646b
David Flemström <david.flemstrom@gmail.com>
2024-07-30 12:29:06 +0200

    Remove faulty downcast of PanicInfo::payload

--- defmt/src/impls/core_/panic.rs
+++ defmt/src/impls/core_/panic.rs
@@ -4,14 +4,9 @@ use super::*;
 
 impl<'a> Format for panic::PanicInfo<'a> {
     fn format(&self, f: Formatter) {
-        crate::write!(f, "panicked at {}:", self.location());
+        crate::write!(f, "panicked at {}", self.location());
         // TODO: consider supporting self.message() once stabilized, or add a crate feature for
         // conditional support
-
-        #[allow(deprecated)] // Deprecated on nightly
-        if let Some(msg) = self.payload().downcast_ref::<&'static str>() {
-            crate::write!(f, "\n{=str}", msg);
-        }
     }
 }
 
