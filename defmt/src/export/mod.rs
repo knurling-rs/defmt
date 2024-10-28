ef797cf22607465f2bde4d3fbe5222bd1222e2eb
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-10-28 20:05:14 +0100

    Merge function calls emitted by the macro to save space.

--- defmt/src/export/mod.rs
+++ defmt/src/export/mod.rs
@@ -209,9 +209,20 @@ pub fn display(val: &dyn core::fmt::Display) {
 }
 
 #[inline(never)]
-pub fn header(s: &Str) {
+pub unsafe fn acquire_and_header(s: &Str) {
+    acquire();
     istr(s);
     timestamp(make_formatter());
+}
+
+#[inline(never)]
+pub fn acquire_header_and_release(s: &Str) {
+    // safety: will be released a few lines further down
+    unsafe { acquire() };
+    istr(s);
+    timestamp(make_formatter());
+    // safety: acquire() was called a few lines above
+    unsafe { release() };
 }
 
 struct FmtWrite;
