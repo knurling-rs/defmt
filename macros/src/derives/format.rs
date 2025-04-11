0e460bcf99887b86047c0abec5e6ffa51cfa7e3c
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-04-11 11:46:26 +0200

    add support for `#[defmt(transparent)]`

--- macros/src/derives/format.rs
+++ macros/src/derives/format.rs
@@ -1,7 +1,8 @@
+use codegen::DefmtAttr;
 use proc_macro::TokenStream;
 use proc_macro_error2::{abort, abort_call_site};
-use quote::{quote, ToTokens};
-use syn::{parse_macro_input, parse_quote, Data, DeriveInput};
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
 
 mod codegen;
 
@@ -14,9 +15,18 @@ pub(crate) fn expand(input: TokenStream) -> TokenStream {
         data,
     } = parse_macro_input!(input as DeriveInput);
 
-    let defmt_path = match defmt_crate_path(&attrs) {
-        Ok(defmt_path) => defmt_path,
-        Err(e) => abort!(e),
+    let DefmtAttr {
+        transparent,
+        defmt_path,
+    } = match attrs
+        .iter()
+        .find(|meta| meta.path().is_ident("defmt"))
+        .cloned()
+        .map(DefmtAttr::try_from)
+        .unwrap_or_else(|| Ok(DefmtAttr::default()))
+    {
+        Ok(maybe_attr) => maybe_attr,
+        Err(err) => abort!(err),
     };
 
     let encode_data = match &data {
@@ -58,24 +68,4 @@ pub(crate) fn expand(input: TokenStream) -> TokenStream {
         }
     )
     .into()
-}
-
-fn defmt_crate_path(attrs: &[syn::Attribute]) -> Result<syn::Path, syn::Error> {
-    let mut defmt_path = parse_quote!(defmt);
-    let res = attrs
-        .iter()
-        .filter(|attr| attr.path().is_ident("defmt"))
-        .try_for_each(|attr| {
-            attr.parse_nested_meta(|meta| {
-                if meta.path.get_ident().is_some_and(|ident| ident == "crate") {
-                    meta.input.parse::<syn::Token![=]>()?;
-                    defmt_path = meta.input.parse::<syn::Path>()?;
-                    Ok(())
-                } else {
-                    let path = meta.path.to_token_stream().to_string().replace(' ', "");
-                    Err(meta.error(format_args!("unknown defmt attribute `{path}`")))
-                }
-            })
-        });
-    res.map(|()| defmt_path)
 }
