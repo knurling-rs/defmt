bce7683b3a05f88372ea7136220c2a41aadbd2f3
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-11-21 15:16:59 +0000

    Update to mdbook@0.5.1
    Also stop using a third-party action to install mdbook in GHA - just
    download it directly.

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd book
-curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.40/mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+curl -L https://github.com/rust-lang/mdBook/releases/download/v0.5.1/mdbook-v0.5.1-x86_64-unknown-linux-gnu.tar.gz | tar xvz
 echo "commit $(git rev-parse --short HEAD) on $(git show -s --format="%ci" HEAD | cut -d" " -f1-2)" >> version.md
 ./mdbook build
