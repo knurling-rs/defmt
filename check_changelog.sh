73ad7bd7a6d144b7f8883d74435df46c0442d820
Konrad Nölle <konrad@familie-noelle.de>
2026-05-21 17:38:01 +0200

    Run check_changelog script in CI

--- check_changelog.sh
+++ check_changelog.sh
@@ -2,7 +2,14 @@
 
 # Checks that all the github links in CHANGELOG.md are correctly defined
 
+status=0
+
 for link in `rg '\[(#\d+)\] ' CHANGELOG.md -o | sort | uniq | sed 's/\]/\\\\]/' | sed 's/\[/\\\\[/'`
 do
-    grep "${link}:" CHANGELOG.md -q || echo ${link//\\/} is MISSING a link-reference, see https://github.github.com/gfm/#shortcut-reference-link
+    if ! rg "${link}:" CHANGELOG.md -q; then
+        echo ${link//\\/} is MISSING a link-reference, see https://github.github.com/gfm/#shortcut-reference-link
+        status=1
+    fi
 done
+
+exit $status
